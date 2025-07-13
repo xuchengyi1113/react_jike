a8148f488b939155dbb40e1df478f1e2f9022d0a
xuchengyi <xuchengyi1113@gmail.com>
2025-07-13 21:56:09 +0800

    tabBar管理

--- react-bill-test/src/pages/Layout/index.js
+++ react-bill-test/src/pages/Layout/index.js
@@ -3,21 +3,55 @@ import { Button } from "antd-mobile";
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { getBillList } from "@/store/modules/billStore";
+import { TabBar } from "antd-mobile";
+import { useNavigate } from 'react-router-dom';
+import './index.scss'
+import {
+  BillOutline,
+  CalculatorOutline,
+  AddCircleOutline
+} from "antd-mobile-icons";
 
+const tabs = [
+  {
+    key: '/',
+    title: '月度账单',
+    icon: <BillOutline />,
+  },
+  {
+    key: '/new',
+    title: '记账',
+    icon: <AddCircleOutline />,
+  },
+  {
+    key: '/year',
+    title: '年度账单',
+    icon: <CalculatorOutline />,
+  },
+];
 const Layout = () => {
   const dispatch = useDispatch();
   useEffect(()=>{
     dispatch(getBillList())
   },[dispatch])
-  return (
-    <div>
-        <Outlet/>
-        I am the layout component
-        <Button color="primary">测试全局</Button>
-        <div className="test">
-            <Button color="primary">测试局部</Button>
 
-        </div>
+  const nagivate = useNavigate();
+  const switchRoute =(path)=>{
+    console.log(path);
+    nagivate(path);
+  }
+  return (
+    <div className="layout">
+      <div className="container">
+        <Outlet/>
+      </div>
+      <div className="footer">
+        <TabBar onChange={switchRoute} defaultActiveKey="/month">
+          {tabs.map((tab) => (
+            <TabBar.Item key={tab.key} title={tab.title} icon={tab.icon}/>
+          ))}
+        </TabBar>
+      </div>
     </div>
     
   );
