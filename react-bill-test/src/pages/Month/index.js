93de011e37e10f6db4bebc8672f7763cf9b9adba
xuchengyi <xuchengyi1113@gmail.com>
2025-07-14 14:01:15 +0800

    月度账单-切换弹窗

--- react-bill-test/src/pages/Month/index.js
+++ react-bill-test/src/pages/Month/index.js
@@ -1,9 +1,51 @@
+import { NavBar, DatePicker } from 'antd-mobile'
+import './index.scss'
+import { useState } from 'react'
+import classNames from 'classnames';
 const Month = () => {
+  const [dataVisible, setDataVisible] = useState(false);
   return (
-    <div>
-        I am the Month component
-    </div>
-  );
-};
+    <div className="monthlyBill">
+      <NavBar className="nav" backArrow={false}>
+        月度收支
+      </NavBar>
+      <div className="content">
+        <div className="header">
+          {/* 时间切换区域 */}
+          <div className="date" onClick={() => setDataVisible(true)}>
+            <span className="text">
+              2023 | 3月账单
+            </span>
+            <span className={classNames('arrow', dataVisible && 'expand')}></span>
+          </div>
+          {/* 统计区域 */}
+          <div className='twoLineOverview'>
+            <div className="item">
+              <span className="money">{100}</span>
+              <span className="type">支出</span>
+            </div>
+            <div className="item">
+              <span className="money">{200}</span>
+              <span className="type">收入</span>
+            </div>
+            <div className="item">
+              <span className="money">{200}</span>
+              <span className="type">结余</span>
+            </div>
+          </div>
+          {/* 时间选择器 */}
+          <DatePicker
+            className="kaDate"
+            title="记账日期"
+            precision="month"
+            visible={dataVisible}
+            onClose={()=>setDataVisible(false)}
+            max={new Date()}
+          />
+        </div>
+      </div>
+    </div >
+  )
+}
 
-export default Month;
+export default Month
